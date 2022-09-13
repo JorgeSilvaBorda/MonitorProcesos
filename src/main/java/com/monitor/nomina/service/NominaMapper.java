c2bcfa6ddf18a214d925ba10ad730fb6787b4c7e
Jorge Silva Borda <jorge.silva8686@gmail.com>
2022-09-13 08:31:36 -0300

    Ajustes en query para traer problemas

--- src/main/java/com/monitor/nomina/service/NominaMapper.java
+++ src/main/java/com/monitor/nomina/service/NominaMapper.java
@@ -23,16 +23,20 @@ public interface NominaMapper {
 	    + "	datediff(MINUTE, b.fecha_proceso, b.fecha_termino) minutos,\n"
 	    + "	b.id_estado idEstado,\n"
 	    + "	c.glosa_estado estado,\n"
-	    + "	convert(date, getdate()) fechaCarga,\n"
 	    + "	getDate() fechaHoraCarga\n"
 	    + "FROM 	\n"
 	    + "	tbl_EmpUnimarc a LEFT JOIN tbl_LogUnimarc b \n"
 	    + "	ON a.id_empresa  = b.id_empresa \n"
 	    + "		and convert(date, b.fecha_proceso) = convert(date, getdate()) LEFT JOIN tbl_EstaUnimarc c\n"
 	    + "	ON b.id_estado = c.id_estado \n"
+	    + "	\n"
 	    + "WHERE \n"
-	    + "	a.estado = 2 \n"
-	    + "		or \n"
-	    + "	(DATEDIFF(MINUTE, CONVERT(TIME, a.per_final), CONVERT(TIME, getdate())) > 0 and b.id_estado is null)")
+	    + "	a.estado = 1 \n"
+	    + "	and (\n"
+	    + "		(DATEDIFF(MINUTE, CONVERT(TIME, a.per_final), CONVERT(TIME, getdate())) > 0 and b.id_estado is null) --Cuando su fecha de proceso es null y su tiempo de ejecución está pasado en minutos\n"
+	    + "			OR \n"
+	    + "		b.id_estado = 2 --Cuando su estado finalizado es error \n"
+	    + "	)\n"
+	    + "ORDER BY a.per_inicio")
     List<ProcesoNomina> getEstadosNominas();
 }
